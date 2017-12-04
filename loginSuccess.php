b81457fb62588925988fd2fa9023cd11e822e115
Adam Decker <adm.decker@gmail.com>
2017-12-03 20:16:41 -0600

    Fixed loginSuccess

--- loginSuccess.php
+++ loginSuccess.php
@@ -1,10 +1,13 @@
+<?php 
+    session_start();
+    if (is_null($_SESSION['userID']))
+        header('/login.html');
+>
 <!DOCTYPE html>
+
+
 <html>
-    <?php 
-        session_start();
-        if (is_null($_SESSION['userID']))
-            header('/login.html');
-    >
+
     <head>
         
     </head>
