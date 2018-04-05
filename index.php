a6b52b80f574fadd0a78644120b8dcd138dcfb87
Snipesy <justin.hoogestraat@jacks.sdstate.edu>
2018-04-05 13:45:34 -0500

    updated cmain to php

--- index.php
+++ index.php
@@ -94,29 +94,3 @@
     </footer>
   </body>
 </html>
-
-
-<?php
-    require "Session.php";
-    
-    PupSession::LoadSession();
-    $location = 'index.html';
-    
-    if (!isset($_SESSION['userType']))
-    {
-        header("Location: /$location");
-        exit();
-    }
-       
-
-    if ($_SESSION["userType"] == "M")
-        $location = 'mindex.html';
-    if ($_SESSION["userType"] == "D")
-        $location = 'dindex.html';
-    if ($_SESSION["userType"] == "C")
-        $location = 'cindex.html';
-
-    header("Location: /$location");
-    
-
-?>
