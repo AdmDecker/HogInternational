a3ff1eaebdbcad44fe1b5159af15f593a9e93450
Snipesy <justin.hoogestraat@jacks.sdstate.edu>
2018-04-08 20:43:33 -0500

    updated logout page

--- logout.php
+++ logout.php
@@ -5,15 +5,45 @@
         PupSession::Destroy();
 ?>
 
-<html>
+<html lang="eng">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="rides-r-us bus system">
+    <meta name="author" content="Justin Hoogestraat">
+    <meta http-equiv="content-type" content="image/svg+xml">
+    <link href="w3.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
 
-    <head>
-        
-    </head>
-    <body>
-        <h1>You have been logged out successfully.</h1>
-        <a href="/index.php">
+  </head>
+  <body>
+    <header class="main-header">
+      <ul class="nav-list">
+
+        <li class="rides-r-us"><a href="index.php"><b>Rides R' Us™</b></a></li>
+      </ul>
+      <hr width="100%">
+    </header>
+    <section>
+      <img src="appicon.svg" class="center" style="width:17em" alt="App Icon Logo">
+
+      <h1 style="text-align:center">You have been logged out successfully.</h1>
+        <a class="center" href="/index.php">
             <button>Return</button>
         </a>
-    </body>
+
+      <br/>
+
+       <hr width="100%">
+    </section>
+
+  
+    <footer>
+      <center>Copyright ©2018 Brookings Area Transit Authority</center>
+      <center>Usage of this site constitues acceptance of our</center>
+      
+       <center> <a href="terms.php">Terms of Service</a>
+        and our
+      <a href="privacy.php">Privacy Policy</a></center>
+    </footer>
+  </body>
 </html>
